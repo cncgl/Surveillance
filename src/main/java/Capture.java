b8bade23cf17d7e6236ac2c396dc2695c08d09bd
Shigeru Kondou <concigel@gmail.com>
2015-08-14 23:46:51 +0900

    init

---
+++ src/main/java/Capture.java
@@ -0,0 +1,29 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.github.sarxos.webcam.Webcam;
+
+
+/**
+ * Example of how to take single picture.
+ *
+ * @author Bartosz Firyn (SarXos)
+ */
+public class Capture {
+
+	public static void main(String[] args) throws IOException {
+
+		// get default webcam and open it
+		Webcam webcam = Webcam.getDefault();
+		webcam.open();
+
+		// get image
+		BufferedImage image = webcam.getImage();
+
+		// save image to PNG file
+		ImageIO.write(image, "PNG", new File("test.png"));
+	}
+}
