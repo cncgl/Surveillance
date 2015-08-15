daf5117044aa54def26c8d812cb37f93aad9a81d
Shigeru Kondou <concigel@gmail.com>
2015-08-15 15:00:30 +0900

    WIP

--- src/main/java/jp/cogel/camera/Capture.java
+++ src/main/java/jp/cogel/camera/Capture.java
@@ -1,8 +1,12 @@
 package jp.cogel.camera;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io.*;
 import java.io.IOException;
+
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.imageio.ImageIO;
 
@@ -14,9 +18,40 @@ import com.github.sarxos.webcam.Webcam;
  *
  * @author Bartosz Firyn (SarXos)
  */
-public class Capture {
+public class Capture extends TimerTask {
+	private static final String PROPERTIES_FILENAME = "capture.properties";
+	private Properties prop = new Properties();
+
+	public Capture() {
+		InputStream in = null;
+		try {
+			in = new BufferedInputStream( new FileInputStream(PROPERTIES_FILENAME) );
+			prop.load(in);
+
+			System.out.println(prop);
+		}catch(IOException ex) {
+			System.err.println(ex.getMessage());
+			ex.printStackTrace();
+		}finally {
+			try {
+				if(in != null) {
+					in.close();
+				}
+			}catch(IOException ex2) {
+				System.err.println(ex2.getMessage());
+				ex2.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+
+	}
 
 	public static void main(String[] args) throws IOException {
+		Capture capture = new Capture();
+
 
 		// get default webcam and open it
 		Webcam webcam = Webcam.getDefault();
