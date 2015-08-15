6fed445cffd27ffa184a0b5cd9ae644f2a3f8d57
Shigeru Kondou <concigel@gmail.com>
2015-08-16 00:19:20 +0900

    いろいろ追加

--- src/main/java/jp/cogel/camera/Capture.java
+++ src/main/java/jp/cogel/camera/Capture.java
@@ -4,9 +4,12 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.io.IOException;
 
+import java.util.Calendar;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import java.text.*;
 
 import javax.imageio.ImageIO;
 
@@ -20,24 +23,36 @@ import com.github.sarxos.webcam.Webcam;
  */
 public class Capture extends TimerTask {
 	private static final String PROPERTIES_FILENAME = "capture.properties";
-	private Properties prop = new Properties();
+	private static final Format FILEDATE_FORMAT = new SimpleDateFormat("YYYYMMddHHmmss");
+	public Properties prop = new Properties();
+
+	private Webcam webcam = Webcam.getDefault();
 
 	public Capture() {
+		try {
+			loadProerties();
+		} catch(Exception ex) {
+			System.out.println(PROPERTIES_FILENAME + " not found.");
+		}
+
+		webcam.open();
+	}
+
+	private void loadProerties() throws Exception {
 		InputStream in = null;
 		try {
 			in = new BufferedInputStream( new FileInputStream(PROPERTIES_FILENAME) );
 			prop.load(in);
 
 			System.out.println(prop);
-		}catch(IOException ex) {
-			System.err.println(ex.getMessage());
-			ex.printStackTrace();
-		}finally {
+		} catch(IOException ex) {
+			throw new Exception(ex.getMessage());
+		} finally {
 			try {
 				if(in != null) {
 					in.close();
 				}
-			}catch(IOException ex2) {
+			} catch(IOException ex2) {
 				System.err.println(ex2.getMessage());
 				ex2.printStackTrace();
 			}
@@ -46,21 +61,27 @@ public class Capture extends TimerTask {
 
 	@Override
 	public void run() {
+		try {
+			// get image
+			BufferedImage image = webcam.getImage();
 
+			// save image to PNG file
+			String filename = prop.getProperty("image.local.filename");
+			int i = filename.indexOf(".");
+			String base = filename.substring(0,i);
+			String ext  = filename.substring(i+1);
+			System.out.println("base:"+base+",ext:"+ext);
+			ImageIO.write(image, "JPEG", new File( base+"-"+FILEDATE_FORMAT.format(Calendar.getInstance().getTime())+"."+ext ));
+		} catch (IOException ex) {
+			System.err.println(ex.getMessage());
+			ex.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
+		Timer t = new Timer();
 		Capture capture = new Capture();
-
-
-		// get default webcam and open it
-		Webcam webcam = Webcam.getDefault();
-		webcam.open();
-
-		// get image
-		BufferedImage image = webcam.getImage();
-
-		// save image to PNG file
-		ImageIO.write(image, "PNG", new File("test.png"));
+		long interval = Long.parseLong( capture.prop.getProperty("ftp.interval") );
+		t.schedule( capture, 0 , interval );
 	}
 }
